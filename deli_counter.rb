e52736fd7576bd5d6b7663c5fc9de43949e1456e
Angela Bardysh <angelabardysh@gmail.com>
2020-05-24 20:15:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,13 @@
 def line(katz_deli)
-  if katz_deli.empty
-    puts "The line is currently empty."
-  else
-    index = 1
-    new_line = []
-    katz_deli.each do |name|
-      new_line.push "#{index}, #{name}"
-      index +=1
-    end
-    puts "The line is currently: #{new_line[0]} #{new_line[1]} #{new_line[2]}"
-  end
+ if katz_deli.empty?
+   puts "The line is currently empty."
+ else
+   index = 1
+   new_line = []
+   katz_deli.each do |name|
+     new_line.push "#{index}. #{name}"
+     index += 1
+   end
+   puts "The line is currently: #{new_line[0]} #{new_line[1]} #{new_line[2]}"
+end
 end
