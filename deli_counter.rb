8f25d75109419408fb5e1942d3a3e7ed50b42aae
Angela Bardysh <angelabardysh@gmail.com>
2020-05-24 20:19:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,3 +11,12 @@ def line(katz_deli)
    puts "The line is currently: #{new_line[0]} #{new_line[1]} #{new_line[2]}"
 end
 end
+
+def take_a_number(katz_deli, name)
+  if katz_deli == []
+    katz_deli.push name
+    puts "Welcome, #{name}. You are number 1 in line."
+  else katz_deli.push name 
+    puts "Welcome, #{name}. You are number 4 in line."
+end 
+end
